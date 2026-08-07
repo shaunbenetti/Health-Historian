04b5a8437a0c7820bb8ac5d8b5fa1e3a54f7cdfe
Shaun Benetti <shaun.benetti@gmail.com>
2026-08-07 15:05:54 -0700

    Initial Commit

---
+++ Health HistorianTests/Health_HistorianTests.swift
@@ -0,0 +1,19 @@
+//
+//  Health_HistorianTests.swift
+//  Health HistorianTests
+//
+//  Created by Shaun Benetti on 8/7/26.
+//
+
+import Testing
+@testable import Health_Historian
+
+struct Health_HistorianTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ Health HistorianUITests/Health_HistorianUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  Health_HistorianUITestsLaunchTests.swift
+//  Health HistorianUITests
+//
+//  Created by Shaun Benetti on 8/7/26.
+//
+
+import XCTest
+
+final class Health_HistorianUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
